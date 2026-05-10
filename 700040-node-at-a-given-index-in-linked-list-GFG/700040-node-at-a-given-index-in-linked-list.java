1118f397a4f2ceee6a2871264d5a796cde6d1043
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-05-10 22:05:01 +0530

    Added solution - LeetHub

---
+++ 700040-node-at-a-given-index-in-linked-list-GFG/700040-node-at-a-given-index-in-linked-list.java
@@ -0,0 +1,28 @@
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int GetNth(Node head, int index) {
+        // Code here
+        
+        int count=1;
+        while(head!=null){
+            if(count==index){
+                return head.data;
+            }
+            count++;
+            head=head.next;
+        }
+        return -1;
+    }
+}
