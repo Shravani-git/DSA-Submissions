d9d1c1b644101523896e21db54c3d064cd14f1d6
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-05-21 20:58:12 +0530

    Added solution - LeetHub

---
+++ 712537-pass-by-reference-and-value-GFG/712537-pass-by-reference-and-value.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+
+class Solution {
+    static int[] passedBy(int a, int b) {
+        // code here
+        return new int[]{a+1,b+2};
+}
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
