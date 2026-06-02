f21ddd6c314e1ed928a858cf5c63b3fcec752a55
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-06-02 19:57:13 +0530

    Added solution - LeetHub

---
+++ 700226--GFG/700226-.java
@@ -0,0 +1,10 @@
+class Solution {
+    int floorSqrt(int n) {
+        // code here
+        return (int)Math.sqrt(n);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
