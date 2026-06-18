763ee300ff45ccd55a7b5a89537cb83de9858ae9
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-06-18 21:07:09 +0530

    Added solution - LeetHub

---
+++ 702755-array-list-insertion-GFG/702755-array-list-insertion.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static ArrayList<Integer> fillArrayList(int arr[]) {
+        // Your code here
+        ArrayList<Integer> ans =  new ArrayList<>();
+        for(int i:arr){
+            ans.add(i);
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
