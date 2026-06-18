a0a9417a37905df9fdc8860fbd4d9f565f1db523
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-06-18 21:05:33 +0530

    Added solution - LeetHub

---
+++ 702999-sort-an-array-list-GFG/702999-sort-an-array-list.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void sortArrayList(ArrayList<Integer> arr) {
+        // code here
+        Collections.sort(arr);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
