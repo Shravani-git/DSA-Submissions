abb66b8242348713755a31e8b98a3b13242c5bd4
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-06-22 20:36:15 +0530

    Added solution - LeetHub

---
+++ 702756--GFG/702756-.java
@@ -0,0 +1,50 @@
+class Solution {
+
+    public static void add_to_ArrayList(ArrayList<Integer> A, int x) {
+        // Your code here
+        A.add(x);
+
+        // Just add, don't print anything
+    }
+
+    public static void sort_ArrayList_Asc(ArrayList<Integer> A) {
+
+        Collections.sort(A);
+        // Your code here
+        // Just sort, don't print anything
+    }
+
+        
+    public static void reverse_ArrayList(ArrayList<Integer> A) {
+        // Your code here
+        // Just reverse, don't print anything
+        Collections.reverse(A);
+    }
+        
+
+    public static int size_Of_ArrayList(ArrayList<Integer> A) {
+        // Your code here
+        // Return the size of ArrayList
+        return A.size();
+    }
+
+    public static void sort_ArrayList_Desc(ArrayList<Integer> A) {
+        // Your code here
+        A.sort(Collections.reverseOrder());
+        
+        // Just sort in desc order, don't print anything
+    }
+
+    public static void print_ArrayList(ArrayList<Integer> A) {
+        // Your code here
+        // Print the elements of A, don't add new line
+        for(Integer i:A){
+            System.out.print(i+" ");
+        }
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
