d25f17ab8561c0eb7bc98161ad32bb5112853a46
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-06-23 20:45:04 +0530

    Added solution - LeetHub

---
+++ 702113--GFG/702113-.java
@@ -0,0 +1,28 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    public static int getLength(Node head) {
+        // code here
+        Node temp = head;
+        int count = 1;
+        while(temp.next!=head){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
