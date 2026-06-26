928dfa52986089c610c9a10e34a1703ca88656e1
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-06-26 20:14:22 +0530

    Added solution - LeetHub

---
+++ 701435-addition-under-modulo-GFG/701435-addition-under-modulo.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int sumUnderModulo(int a, int b, int M) {
+        // code here
+        return (a+b)%M ;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
