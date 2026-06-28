c8fb3a3c76c8fb77177c2530f8c0722d96106a78
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-06-28 20:49:21 +0530

    Added solution - LeetHub

---
+++ 703289-segregate-0s-and-1s-GFG/703289-segregate-0s-and-1s.java
@@ -0,0 +1,12 @@
+class Solution {
+    void segregate0and1(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
