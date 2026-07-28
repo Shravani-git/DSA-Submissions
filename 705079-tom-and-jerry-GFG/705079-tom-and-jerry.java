e3eb589896e72d19fc475d080e9eb2652d71883d
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-07-28 20:25:33 +0530

    Added solution - Time: O(1), Space: O(1)

---
+++ 705079-tom-and-jerry-GFG/705079-tom-and-jerry.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int numsGame(int N) {
+        // code here
+        if(N%2==0){
+            return 1;
+        }
+        return 0;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
