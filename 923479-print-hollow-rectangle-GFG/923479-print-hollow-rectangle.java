cdefe64016294a5c6df000bd8b05afef87c5d5ce
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-08-02 20:37:49 +0530

    Time: O(n*m), Space: O(1)

---
+++ 923479-print-hollow-rectangle-GFG/923479-print-hollow-rectangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1 || i==n || j==1 || j==m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
