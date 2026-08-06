08b2e8474a881009ecf54d245f249218588b98ae
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-08-06 20:39:48 +0530

    Added solution - Time: O(n), Space: O(n)

---
+++ 701236-indexes-of-subarray-sum-GFG/701236-indexes-of-subarray-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        ArrayList<Integer> result = new ArrayList<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int sum = 0;
+        map.put(0, -1); // important edge case
+        
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            
+            if (map.containsKey(sum - target)) {
+                result.add(map.get(sum - target) + 2);
+                result.add(i+1);
+                return result;
+            }
+            
+           if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        
+        result.add(-1);
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
