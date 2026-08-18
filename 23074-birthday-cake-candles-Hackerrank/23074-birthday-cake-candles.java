7bacfc5d8359df3af3bed6e576594f65dd8bf595
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-08-18 22:39:49 +0530

    Added Solution - Time: O(n), Space: O(n)

---
+++ 23074-birthday-cake-candles-Hackerrank/23074-birthday-cake-candles.java
@@ -0,0 +1,28 @@
+h.put(i,1);
+        }
+        else{
+            max=Math.max(max,i);
+            h.put(i,x+1);
+        }
+            int x = h.get(i);
+    for(int i:candles){
+        if(h.containsKey(i)){
+    int max=-1;
+    new HashMap<>();
+    HashMap<Integer,Integer> h = 
+    // Write your code here
+    (List<Integer> candles) {
+    birthdayCakeCandles
+    public static int 
+
+     */
+     parameter.
+     INTEGER_ARRAY candles as 
+     * The function accepts 
+     to return an INTEGER.
+     * The function is expected 
+     *
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
