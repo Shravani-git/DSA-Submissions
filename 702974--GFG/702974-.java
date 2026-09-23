d3c6696b1320de19e8923a387c89a023cde4f3fc
Shravani <160341618+Shravani-git@users.noreply.github.com>
2026-09-23 22:21:53 +0530

    Added solution - Time: O(n), Space: O(1)

---
+++ 702974--GFG/702974-.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIdentical(int a[], int b[]) {
+        // code here
+        return Arrays.equals(a, b);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
